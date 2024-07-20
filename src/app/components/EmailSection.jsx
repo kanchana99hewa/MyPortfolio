4748f0196e1d9acc13650c7fb52c88d1ebef338a
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-07-20 11:18:10 +0530

    first

---
+++ src/app/components/EmailSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EmailSection() {
+  return (
+    <div>EmailSection</div>
+  )
+}
+
+export default EmailSection
---
+++ src/app/components/ProjectTag.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProjectTag() {
+  return (
+    <div>ProjectTag</div>
+  )
+}
+
+export default ProjectTag
