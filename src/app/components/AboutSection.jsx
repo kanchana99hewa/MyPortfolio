90aa0a70723cdd3b1cc56d899dfbf6b7206097fe
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-08-25 16:29:48 +0530

    skills_columns

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -3,21 +3,34 @@ import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
 
+const SKILL_DATA = [
+  { name: "HTML", percentage: 90 },
+  { name: "CSS", percentage: 85 },
+  { name: "Tailwind", percentage: 80 },
+  { name: "Bootstrap", percentage: 75 },
+  { name: "JavaScript", percentage: 80 },
+  { name: "React", percentage: 85 },
+  { name: "Next.js", percentage: 75 },
+];
+
 const TAB_DATA = [
   {
     title: "Skills",
     id: "skills",
     content: (
-      <ul className="pl-2 list-disc">
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>C</li>
-        <li>Java</li>
-        <li>Tailwind CSS</li>
-        <li>ReactJS</li>
-        <li>NextJS</li>
-      </ul>
+      <div className="space-y-4">
+        {SKILL_DATA.map((skill, index) => (
+          <div key={index} className="flex items-center">
+            <div className="w-24 font-semibold">{skill.name}</div>
+            <div className="w-full h-6 overflow-hidden bg-gray-700 rounded-full">
+              <div
+                className="h-full rounded-full bg-gradient-to-r from-pink-400 to-purple-500"
+                style={{ width: `${skill.percentage}%` }}
+              ></div>
+            </div>
+          </div>
+        ))}
+      </div>
     ),
   },
   {
@@ -37,7 +50,6 @@ const TAB_DATA = [
       <ul className="pl-2 list-disc">
         <li>Sujatha Vidyalaya Matara</li>
         <li>BSc.Honours Degree in Computing and Information Systems</li>
-        
       </ul>
     ),
   },
@@ -72,13 +84,13 @@ const AboutSection = () => {
         <div className="flex flex-col h-full mt-4 text-left md:mt-0">
           <h2 className="mb-4 text-4xl font-bold text-white">About Me</h2>
           <p className="text-base lg:text-lg">
-          I am a UI UX designer and a frontend web developer. I am very passionate about UI/UX design, which 
-          has led me to work on several projects, like group projects as 
-          well as individual projects. Through this experience, I was able  
-          to develop skills in user research, wireframing, and prototyping. 
-          I am proficient with tools like Figma, Adobe XD, and Sketch, and 
-          I have a good understanding of front-end development, including 
-          HTML, CSS, JavaScript, ReactJs, NextJs. 
+            I am a UI UX designer and a frontend web developer. I am very passionate about UI/UX design, which
+            has led me to work on several projects, like group projects as
+            well as individual projects. Through this experience, I was able
+            to develop skills in user research, wireframing, and prototyping.
+            I am proficient with tools like Figma, Adobe XD, and Sketch, and
+            I have a good understanding of front-end development, including
+            HTML, CSS, JavaScript, ReactJs, NextJs.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
