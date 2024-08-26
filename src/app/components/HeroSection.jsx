d5a5a213da4e0b425548aa59d02602aed0186143
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-08-26 18:48:59 +0530

    .

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -19,13 +19,12 @@ const HeroSection = () => {
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
-            <br></br>
+            <br />
             <TypeAnimation
               sequence={[
                 "Kanchana",
                 1000,
                 "Web Developer",
-           
                 1000,
                 "UI/UX Designer",
                 1000,
@@ -36,24 +35,28 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-          Passionate about design and enhancing user experiences, I create intuitive and visually compelling interfaces
+            Passionate about design and enhancing user experiences, I create intuitive and visually compelling interfaces.
           </p>
+          
           <div>
             <Link
               href="/#contact"
-              className="inline-block w-full px-6 py-3 mr-4 text-white rounded-full sm:w-fit bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200"
+              className="inline-block w-full px-6 py-3 mr-4 font-bold text-white rounded-full sm:w-fit bg-gradient-to-br from-primary-900 to-secondary-700 hover:bg-slate-200"
             >
               Hire Me
             </Link>
+            
             <Link
               href="/"
-              className="inline-block w-full px-1 py-1 mt-3 text-white rounded-full sm:w-fit bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800"
+              className="inline-block w-full px-1 py-1 mt-3 text-black rounded-full sm:w-fit bg-gradient-to-br from-primary-900 to-secondary-500 hover:bg-slate-900"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block bg-[#ffffff] hover:bg-slate-800 rounded-full px-5 py-2 font-bold">
                 Download CV
               </span>
             </Link>
+            
           </div>
+          
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
@@ -61,14 +64,17 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 mt-4 place-self-center lg:mt-0"
         >
-          <div className="rounded-full bg-[#722360] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/hero'''image - Copy.png"
-              alt="hero image"
-              className="absolute transform -translate-y-1/2 -translate-x-1/4 top-1/2 left-1/4"
-              width={500}
-              height={500}
-            />
+          <div className="relative w-[300px] h-[300px] lg:w-[500px] lg:h-[500px]">
+            <div className="absolute inset-0 bg-gradient-to-br from-[#962265] to-[#c24dc2] blur-[100px] rounded-full"></div>
+            <div className="relative w-full h-full rounded-full">
+              <Image
+                src="/images/hero'''image - Copy.png"
+                alt="hero image"
+                className="absolute transform -translate-y-1/2 -translate-x-1/4 top-1/2 left-1/4"
+                width={500}
+                height={500}
+              />
+            </div>
           </div>
         </motion.div>
       </div>
