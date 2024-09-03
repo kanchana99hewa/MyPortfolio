265b1efa3995dd2ff96b5a5509752c5218dc4598
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-09-03 12:53:18 +0530

    volunteering images

--- src/app/components/Volunteering.jsx
+++ src/app/components/Volunteering.jsx
@@ -6,52 +6,53 @@ const volunteeringData = [
   {
     title: "Volunteer Team Member for ICARC",
     description: "Annual International Conference Organized by FOC, SUSL",
-    imageUrl: "/images/beach-cleanup.jpg", 
+    imageUrl: "/images/ica.jpeg", 
   },
   {
-    title: "Initiative Group Vice Presidentfor Marketing & Digital Experience",
-    description: " AIESEC in SUSL",
-    imageUrl: "/images/tree-planting.jpg", 
-  {
-    title: "Committee member ",
-    description: " IEEE WIE Affinity Group",
-    imageUrl: "/images/food-donation.jpg", 
+    title: "Initiative Group Vice President for Marketing & Digital Experience-23.24",
+    description: "AIESEC in SUSL",
+    imageUrl: "/images/aie.jpeg", 
   },
   {
-    title: "Member ",
-    description: " IEEE Student Branch ",
-    imageUrl: "/images/food-donation.jpg", 
+    title: "Committee member",
+    description: "IEEE WIE Affinity Group",
+    imageUrl: "/images/wi.jpeg", 
   },
   {
-    title: "Member ",
-    description: " Society of Computer Sciences  ",
-    imageUrl: "/images/food-donation.jpg",
+    title: "Member",
+    description: "IEEE Student Branch",
+    imageUrl: "/images/iee.jpeg", 
   },
   {
-    title: "Design team  ",
-    description: " IEEE WIE Affinity Group  ",
-    imageUrl: "/images/food-donation.jpg", 
+    title: "Member",
+    description: "Society of Computer Sciences",
+    imageUrl: "/images/soc.png", 
+  },
+  {
+    title: "Design team",
+    description: "IEEE WIE Affinity Group",
+    imageUrl: "/images/wides.jpeg", 
   },
 ];
 
 const Volunteering = () => {
   return (
-    <section className="min-h-screen px-4 py-16 text-gray-800">
+    <section className="min-h-screen px-4 py-16 text-gray-900">
       <div className="container mx-auto">
         <h1 className="mb-12 text-4xl font-bold text-center text-gradient">My Volunteering Experience</h1>
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <div className="flex flex-wrap justify-center gap-4">
           {volunteeringData.map((volunteer, index) => (
-            <div key={index} className="bg-white rounded-lg shadow-lg">
+            <div key={index} className="flex flex-col justify-between bg-purple-300 rounded-lg shadow-lg w-52">
               <Image
                 src={volunteer.imageUrl}
                 alt={volunteer.title}
-                width={500}
-                height={300}
-                className="object-cover rounded-t-lg"
+                width={208}
+                height={130}
+                className="object-contain rounded-t-lg"
               />
-              <div className="p-4">
-                <h2 className="mb-2 text-xl font-semibold">{volunteer.title}</h2>
-                <p className="text-gray-600">{volunteer.description}</p>
+              <div className="flex flex-col justify-start p-3 min-h-32">
+                <h2 className="mb-1 text-lg font-semibold text-center">{volunteer.title}</h2>
+                <p className="text-sm text-center text-gray-900">{volunteer.description}</p>
               </div>
             </div>
           ))}
