00feac399dcf6df07e1c525db60b3ddad1e063f8
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2026-05-20 22:54:06 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
-
   basePath: isProd ? "/MyPortfolio" : undefined,
-
+  assetPrefix: isProd ? "/MyPortfolio/" : undefined,  
   images: {
     unoptimized: true,
   },
